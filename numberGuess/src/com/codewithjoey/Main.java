b61439d89adb2b43ff78f833480c8322b10abcee
Joseph Tharpe <joeytharpe23@hotmail.com>
2022-03-22 11:05:35 -0700

    updated number guesser

--- numberGuess/src/com/codewithjoey/Main.java
+++ numberGuess/src/com/codewithjoey/Main.java
@@ -7,26 +7,22 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Scanner scanner = new Scanner(System.in);
+        double numb = Math.floor(Math.random() * 20);
+        System.out.println("Hello what is your name ?");
+        String name = scanner.nextLine();
+        System.out.println("well hello " + name + ", I am thinking of a number between 1 and 20.");
+        System.out.println("Take a guess.");
 
-        System.out.println("You are in a land full of Dragons. In front of you,");
-        System.out.println("you see two caves. In one cave, the dragon is friendly");
-        System.out.println("and will share his treasures with you. The other dragon");
-        System.out.println("is greedy and hungry and will eat you on sight.");
-        System.out.print("Which cave will you go into? (1 or 2) :");
-
-        int answer = scanner.nextInt();
-        if(answer == 1){
-            System.out.println("You approach the cave...");
-            System.out.println("it is dark and spooky...");
-            System.out.println("A large dragon jumps out in front of you! He opens his jaws and...");
-            System.out.println("Gobbles you down in one bite !");
-        } else if(answer == 2) {
-            System.out.println("You approach the cave...");
-            System.out.println("it is not so dark and spooky...");
-            System.out.println("A large dragon jumps out in front of you! He opens his hands and...");
-            System.out.println("Gives you lots of gold and food !");
-        } else {
-            System.out.println("Sorry wrong number u cant escape the dragons would u like to retry ?");
+        while(true){
+            double inputNum = scanner.nextDouble();
+            if(inputNum > numb){
+                System.out.println("your guess is too high");
+            } else if(inputNum < numb){
+                System.out.println("your guess is too low");
+            } else {
+                System.out.println("Correct !");
+                break;
+            }
         }
     }
 }
